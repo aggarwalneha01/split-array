f0bcb9cd4cf7eb8f7419319272f991fae49503cc
Neha Aggarwal <neha.aggarwal@bbc.co.uk>
2021-08-09 01:55:23 +0100

    refactor

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
 
-function groupArrayElements(origArray, numOfArrays){
+const  groupArrayElements = (origArray, numOfArrays) => {
   const result = [];
-  const newArraySize = -Math.round(-(origArray.length / numOfArrays))
-  if(newArraySize===0){
+  const newArraySize = - Math.round(-(origArray.length / numOfArrays)) // This is done so that .5 is rounded to the lower number ex (1.5 -> 1)
+  if(newArraySize === 0){
     return Error("Could not divide an empty array");
   }
-  else if(numOfArrays>origArray.length){ 
-    console.log("Can not divide array of length" , origArray.length, "in to", numOfArrays, "parts");
-    console.log(origArray);
+
+ if(numOfArrays > origArray.length){ 
     return origArray;
   }
   else {   
-  let startIndex = 0;
-  let endIndex = newArraySize;
-  for (let i =0; i < numOfArrays; i ++){
-    if (i===numOfArrays-1){
-      result.push(origArray.slice(startIndex,origArray.length));
+    let startIndex = 0;
+    let endIndex = newArraySize;
+    for (let i = 0; i < numOfArrays; i ++){
+      if (i === numOfArrays - 1){
+        result.push(origArray.slice(startIndex, origArray.length));
+      }
+      else{
+      result.push(origArray.slice(startIndex, endIndex));
+      }
+      startIndex = endIndex;
+      endIndex = startIndex+newArraySize;
     }
-    else{
-    result.push(origArray.slice(startIndex, endIndex));
-    }
-    startIndex=endIndex;
-    endIndex = startIndex+newArraySize;
+    return result;
   }
-  console.log(result);
-  return result;
-}
 }
 
 export default groupArrayElements;
